8518da951218b7dafb6bbca09a7cd1c8d06d9c40
s-omoto23 <soheiomoto@gmail.com>
2024-08-23 16:42:03 +0900

    [update] supported beam-eye-tracker module

--- eyegaze.py
+++ eyegaze.py
@@ -8,11 +8,18 @@
   - NumPy
 """
 
+import os
 import sys
-from eyeware.client import TrackerClient
-from datetime import datetime
 import time
 import numpy as np
+from datetime import datetime
+
+#SDKがあるディレクトリにPATHを通す
+path = os.getcwd()
+path_name = path + "\\BeamSDK-Windows64-1.1.0\\API\\python"
+sys.path.append(path_name)
+
+from eyeware.client import TrackerClient
 
 tracker = TrackerClient()
 
@@ -24,14 +31,15 @@ def gaze_tracker():
     with open(name, 'w') as f:
         while True:
             if tracker.connected:
+                record_now = datetime.now()
+                record_time = record_now.strftime("%H%M%S")
+                
                 head_pose = tracker.get_head_pose_info()
                 head_is_lost = head_pose.is_lost
                 if not head_is_lost:
                     rot = head_pose.transform.rotation
                     tr = head_pose.transform.translation
 
-                record_now = datetime.now()
-                record_time = record_now.strftime("%H%M%S")
                 print(record_time, file=f)
                 print("  * Gaze on Screen:",file=f)
                 screen_gaze = tracker.get_screen_gaze_info()
@@ -39,7 +47,6 @@ def gaze_tracker():
                 print("      - Lost track:       ", screen_gaze_is_lost,file=f)
                 if not screen_gaze_is_lost:
                     print("      - Coordinates:       <x=%5.3f px,   y=%5.3f px>" % (screen_gaze.x, screen_gaze.y),file=f)
-                    print("      - Confidence:       ", screen_gaze.confidence,file=f)
                 
                 #30hzでデータを取得
                 time.sleep(1 / 30)
@@ -47,7 +54,7 @@ def gaze_tracker():
                 #APIとの通信が失われている場合は1秒おきにエラー文をプリント
                 MESSAGE_PERIOD_IN_SECONDS = 1
                 time.sleep(MESSAGE_PERIOD_IN_SECONDS - time.monotonic() % MESSAGE_PERIOD_IN_SECONDS)
-                print("No connection with tracker server",file=f)
+                print("No connection with tracker server")
 
 if __name__ == "__main__":
     gaze_tracker()
